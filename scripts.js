6c8ede09d1e7b9c9c50d67bcfcde0a45507cc19f
juan camilo <camilo.mro@gmail.com>
2025-01-27 16:29:17 +0000

    Add main functionalities -add function to check if the hand is busted(over 21) -add function for dealer's hand logic -function to reset the buttons on new game -function to determine the winner given 2 hands -Add event listener for 4 buttons: hit,stand,newgame,dealcards

--- scripts.js
+++ scripts.js
@@ -7,21 +7,25 @@
 // 7- Determine the winner
 // 8- Play again
 
-const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
-const ranks = [ "Ace","2", "3", "4", "5", "6", "7", "8", "9", "10","Jack", "Queen", "King"];
 
-
-const deck = [];
-
-// Generate the deck
-suits.forEach(suit => {
-  ranks.forEach(rank => {
-    deck.push({ rank, suit });
+//Create deck
+//Return order list obj
+let createDeck = () =>{
+  const deck = [];
+  const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+  const ranks = [ "Ace","2", "3", "4", "5", "6", "7", "8", "9", "10","Jack", "Queen", "King"];
+  
+  // Generate the deck
+  suits.forEach(suit => {
+    ranks.forEach(rank => {
+      deck.push({ rank, suit });
+    });
   });
-});
-
+  return deck;
+}
 
 //Fisher-Yates Shuffle Function to Shuffle the deck
+//return deck list obj
 let Shuffle = (deck) => {
   for (let i = deck.length - 1; i > 0; i--) {
 
@@ -32,35 +36,11 @@ let Shuffle = (deck) => {
   return deck;
 }
 
-//Second option to shuffle deck
-//deck.sort(() => Math.random() - 0.5);
-
-//Shuffle the deck
-Shuffle(deck);
-
-//Function to deal first 3 cards
-let dealCards = (cards, numCards) => {
-  let playersHand = [];
-  let dealersHand = [];
-  
-  for (let i = 0; i < numCards; i++){
-    if(i % 2 == 0){
-      playersHand.push(cards.shift());
-    }else{
-      dealersHand.push(cards.shift());
-    }
-    
-  }
-  return {playersHand, dealersHand};
-}
-
-//Deal first 3 cards of the game
-let { playersHand, dealersHand } = dealCards(deck, 3)
 
 
 
 //Function to calculate value of the card
-//Return int or array of ints
+//Return int 
 let valueCard = (card) => {
   let value = 0;
   if (card.rank == "Ace"){
@@ -73,20 +53,174 @@ let valueCard = (card) => {
   return value;
 }
 
-/*
+//Calculate score of a hand
+//return total score
 let calculateScore = (hand) => {
+  let aces = 0;
   let score = 0;
   for(card in hand){
-    score += valueCard(hand[card]);      
+    score += valueCard(hand[card]);   
+    if(hand[card].rank == "Ace"){
+      aces++;
+    }   
+  }
+  while(score > 21 && aces > 0){
+    score -= 10;
+    aces--;
   }
   return score;
 }
-*/
+
 
 //Function to get card from deck
+//updates deck and hand
 let drawCard = (hand, deck) => {
   let card = deck.shift();
   hand.push(card);
-  return card;
 }
+
+//Function to check if the hand is blackJack
+//return boolean
+let isBlackjack = (hand) =>{
+  return calculateScore(hand) == 21 && hand.length == 2;
+}
+//function to check if the hand is bust
+//return boolean
+let isBust = (hand) =>{
+  return calculateScore(hand) > 21;
+}
+
+
+//Function to deal first 3 cards
+let dealCards = (deck) => {
+  let playersHand = [];
+  let dealersHand = [];
+  
+  for (let i = 0; i < 3; i++){
+    if(i % 2 == 0){
+      playersHand.push(deck.shift());
+    }else{
+      dealersHand.push(deck.shift());
+    }
+
+  }
+  return {playersHand, dealersHand};
+
+}
+
+//Function takes card on 16 and stay on 17
+//No return value
+let dealersPlay = (dealersHand, deck) => {
+  while(calculateScore(dealersHand) < 17 && !isBust(dealersHand)){
+    drawCard(dealersHand, deck);
+  } 
+}
+
+let resetButtons = () => {
+  hitButton.disabled = true;
+  standButton.disabled = true;
+  dealButton.disabled = true;
+  newGameButton.disabled = false;
+}
+
+//Function to determine the winner of the game
+//return string
+let determineWinner = (playersHand, dealersHand) => {
+  if (isBust(playersHand)) return "Player Busts! Dealer Wins!";
+  if (isBust(dealersHand)) return "Dealer Busts! Player Wins!";
+
+  let playerScore = calculateScore(playersHand);
+  let dealerScore = calculateScore(dealersHand);
+  if (playerScore > dealerScore) return "Player Wins!";
+  if (playerScore < dealerScore) return "Dealer Wins!";
+  
+  return "It's a Tie!";
+}
+
+
+const deck = createDeck();
+
+let playersHand = [];
+let dealersHand = [];
+
+let dealersHandDiv = document.getElementById("dealer-cards");
+let playersHandDiv = document.getElementById("player-cards");
+let messageDiv = document.getElementById("message");
+let resultDiv = document.getElementById("result");
+    
+let newGameButton = document.getElementById("new-game-button");
+let dealButton = document.getElementById("deal-button");
+let hitButton = document.getElementById("hit-button");
+let standButton = document.getElementById("stand-button");
+
+resetButtons();
+//Disable buttons at start
+
+
+//NEW GAME
+newGameButton.addEventListener("click", () => {
+  //Empty both deal and players hands
+  playersHand = [];
+  dealersHand = [];
+  //Create a new deck and shuffle it
+  Shuffle(deck);
+  
+  resultDiv.innerHTML = "";
+  //Empty HTML
+  playersHandDiv.innerHTML = "";
+  dealersHandDiv.innerHTML = ""; 
+  dealButton.disabled = false;
+})
+
+//DEAL CARDS
+dealButton.addEventListener("click", () => {
+  
+  ({ playersHand, dealersHand } = dealCards(deck));
+
+  //Display dealer's hand
+  dealersHandDiv.innerHTML = JSON.stringify(dealersHand) + "Score: " + calculateScore(dealersHand);
+
+  //Display player's hand
+  playersHandDiv.innerHTML = JSON.stringify(playersHand) + "Score: " + calculateScore(playersHand);
+
+  dealButton.disabled = true;
+  newGameButton.disabled =true;
+
+  hitButton.disabled = false;
+  standButton.disabled = false;
+  
+})
+
+//STAND
+standButton.addEventListener("click", () => {
+
+  //Dealer's turn
+  dealersPlay(dealersHand,deck);
+  dealersHandDiv.innerHTML = JSON.stringify(dealersHand) + "Score: " + calculateScore(dealersHand);
+  let result = determineWinner(playersHand, dealersHand);
+  document.getElementById("result").innerHTML = result;
+  resetButtons();
+});
+
+
+//HIT
+hitButton.addEventListener("click", () => {
+  drawCard(playersHand, deck);
+  let playersScore = calculateScore(playersHand);
+
+  playersHandDiv.innerHTML = JSON.stringify(playersHand) + "Score: " + playersScore;
+
+  if(isBust(playersHand)){
+    resultDiv.innerHTML = "Player Busts! Dealer Wins!";
+    resetButtons();
+
+  }else if(playersScore == 21){
+    dealersPlay(dealersHand,deck);
+    dealersHandDiv.innerHTML = JSON.stringify(dealersHand) + "Score: " + playersScore;
+    let result = determineWinner(playersHand, dealersHand);
+    resultDiv.innerHTML = result;
+    resetButtons();
+  }
+  
+});
 
